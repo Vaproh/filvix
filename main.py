291328c54f9a764ea066f5b67d52d3b39e727ad4
Vaproh <vaproh@proton.me>
2024-04-01 20:12:29 +0530

    Co-authored-by: Cloud9 <yuvrajkushwahh@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.errors import MissingPermissions
-#import os
+import os
 import json
 #import jiskhau
 import config
+#import cogs
+import time
 
 
 
@@ -15,6 +17,12 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_connect():
   await bot.change_presence(status=discord.Status.dnd,activity=discord.Activity(type=discord.ActivityType.listening,name="Cloudy "))
 
+def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            cogs = filename[:-2]
+            bot.load_extension(f'cogs.{"general"}')
+            bot.load_extension(f'cogs.{"moderation"}')
 
 
 @bot.event
@@ -25,6 +33,12 @@ async def on_ready():
     print(f"logged In as {bot.user.name}")
     print(f"Total servers ~ {len(bot.guilds)}")
     print(f"Total Users ~ {len(bot.users)}")
+    await bot.load_extension("cogs.moderation") # loading moderation module
+    print("Loaded module Moderation")
+    time.sleep(0.5)
+    print("Registering Slash Commands")
+    await bot.tree.sync() # register slash commands
+    print("Registerd all Slash Commands")
 
 
 
@@ -48,49 +62,46 @@ async def on_ready():
 
 
 
-@commands.has_permissions(manage_messages=True)
-@bot.hybrid_command(name='purge', brief='Deletes a specified number of messages from the current channel')
-async def purge(ctx, amount: int):
-  # Delete the specified number of messages
-  deleted = await ctx.channel.purge(limit=amount)
-  if len(deleted) == 0:
-    # If no messages were deleted, create an embed message with a custom color and text
-    embed = discord.Embed(title='Purge complete', color=0xFFFF00)
-    embed.description = 'No messages were deleted'
-    # Set the user's profile picture as the thumbnail of the embed
-    embed.set_thumbnail(url=ctx.author.avatar.url)
-    # Send the embed message
-    await ctx.send(embed=embed)
-  else:
-    # Create an embed message with a custom color and text
-    embed = discord.Embed(title='Purge complete', color=0xFFFF00)
-    if len(deleted) == 1:
-      # If only one message was deleted, use singular text
-      embed.description = '1 message was deleted'
-    else:
-      # If more than one message was deleted, use plural text
-      embed.description = f'{len(deleted)} messages were deleted'
-    # Set the user's profile picture as the thumbnail of the embed
-    embed.set_thumbnail(url=ctx.author.avatar.url)
-    # Send the embed message
-    await ctx.send(embed=embed)
+# @commands.has_permissions(manage_messages=True)
+# @bot.hybrid_command(name='purge', brief='Deletes a specified number of messages from the current channel')
+# async def purge(ctx, amount: int):
+#   # Delete the specified number of messages
+#   deleted = await ctx.channel.purge(limit=amount)
+#   if len(deleted) == 0:
+#     # If no messages were deleted, create an embed message with a custom color and text
+#     embed = discord.Embed(title='Purge complete', color=0xFFFF00)
+#     embed.description = 'No messages were deleted'
+#     # Set the user's profile picture as the thumbnail of the embed
+#     embed.set_thumbnail(url=ctx.author.avatar.url)
+#     # Send the embed message
+#     await ctx.send(embed=embed)
+#   else:
+#     # Create an embed message with a custom color and text
+#     embed = discord.Embed(title='Purge complete', color=0xFFFF00)
+#     if len(deleted) == 1:
+#       # If only one message was deleted, use singular text
+#       embed.description = '1 message was deleted'
+#     else:
+#       # If more than one message was deleted, use plural text
+#       embed.description = f'{len(deleted)} messages were deleted'
+#     # Set the user's profile picture as the thumbnail of the embed
+#     embed.set_thumbnail(url=ctx.author.avatar.url)
+#     # Send the embed message
+#     await ctx.send(embed=embed)
 
 
 
 
 
-@bot.event
-async def on_command_error(ctx, error):
-  if isinstance(error, commands.MissingPermissions):
-    await ctx.send(
-      f"{ctx.author.mention} You do not have enough permissions to use the `{ctx.command}` command."
-    )
-  elif isinstance(error, commands.CommandNotFound):
-    pass  # do nothing if the command doesn't exist
-  else:
-    print(f"Error occurred: {str(error)}")     
+# @bot.event
+# async def on_command_error(ctx, error):
+#   if isinstance(error, commands.MissingPermissions):
+#     await ctx.send(
+#       f"{ctx.author.mention} You do not have enough permissions to use the `{ctx.command}` command."
+#     )
+#   elif isinstance(error, commands.CommandNotFound):
+#     pass  # do nothing if the command doesn't exist
+#   else:
+#     print(f"Error occurred: {str(error)}")     
 
 bot.run(config.token)
-
-
-
