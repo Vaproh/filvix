19a9ebc7b9b8db6c2a2fd2b4a4d65eb1f0ffb08e
Bazzi <bazziprivate13@gmail.com>
2024-04-06 20:27:39 +0530

    Added Avatar Command

--- commands/general.py
+++ commands/general.py
@@ -8,35 +8,14 @@ class General(commands.Cog):
     def __init__(self, bot: CustomBot):
         self.bot = bot
 
-    @commands.hybrid_command(
-            usage="Avatar [member]",
-            name='avatar',
-            aliases=['av', 'ac', 'pfp', 'ico'],
-            help="""Wanna steal someone's avatar here you go
-    Aliases""")
-    async def user(self,
-                        ctx,
-                        member: Optional[Union[discord.Member,
-                                            discord.User]] = None):
-            if member == None or member == "":
-                member = ctx.author
-            user = await self.bot.fetch_user(member.id)     
-            webp = user.avatar.replace(format='webp')
-            jpg = user.avatar.replace(format='jpg')
-            png = user.avatar.replace(format='png')
-            embed = discord.Embed(
-                    color=random.randint(0, 0xFFFFFF),
-                    description=f"[`PNG`]({png}) | [`JPG`]({jpg}) | [`WEBP`]({webp})"
-                    if not user.avatar.is_animated() else
-                    f"[`PNG`]({png}) | [`JPG`]({jpg}) | [`WEBP`]({webp}) | [`GIF`]({user.avatar.replace(format='gif')})"
-                )
-            embed.set_author(name=f"{member}",
-                                icon_url=member.avatar.url
-                                if member.avatar else member.default_avatar.url)
-            embed.set_image(url=user.avatar.url)
-            embed.set_footer(
-                    text=f"Requested By {ctx.author}",
-                    icon_url=ctx.author.avatar.url
-                    if ctx.author.avatar else ctx.author.default_avatar.url)
-                
-            await ctx.send(embed=embed)
+    # avatar command
+    @commands.hybrid_command(name='avatar', help='Displays the user\'s avatar.',aliases=['av'])
+    async def avatar(self, ctx, member: Optional[Union[discord.User, discord.Member]] = None):
+        user = user or ctx.author
+        embed = discord.Embed(title=f"{self.user.name}'s Avatar", color=0x0565ff)
+        embed.set_image(url=self.author.avatar_url)
+        await ctx.send(embed=embed)
+
+        # add cogs
+async def setup(bot: CustomBot) -> None:
+    await bot.add_cog(General(bot))
